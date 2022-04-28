f201f0999dff23abb639b53bbb3c82ef277f44d1
Rasmus <rasmuswium@hotmail.com>
2022-04-28 17:21:00 +0200

    Handler not working

---
+++ dotnetHandler/Db.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetHandler
+{
+    public class HotelContext : DbContext
+    {
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public string DbPath { get; }
+
+        public HotelContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "hotel.db");
+            Console.WriteLine(DbPath);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class Reservation
+    {
+        public int hotelId;
+        public string checkIn;
+        public string checkOut;
+        public int roomNo;
+        public string customerName;
+        public string customerEmail;
+        public string customerAddress;
+    }
+}
--- dotnetHandler/Program.cs
+++ dotnetHandler/Program.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Text.Json;
 
 namespace dotnetHandler
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      var db = new HotelContext();
+      var factory = new ConnectionFactory() { HostName = "hawk.rmq.cloudamqp.com", UserName = "xdcsxmjy", VirtualHost = "xdcsxmjy", Password = "WEhsUfZPjDfyw8qatdH81MCfa8wB0xfK" };
+
+      using (var connection = factory.CreateConnection())
+      {
+        var channelConfirmations = connection.CreateModel();
+
+        channelConfirmations.QueueDeclare(queue: "confirmations",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+        using (var channelReservations = connection.CreateModel())
         {
-            Console.WriteLine("Hello World!");
-        }
+          channelReservations.QueueDeclare(queue: "reservations",
+              durable: true,
+              exclusive: false,
+              autoDelete: false,
+              arguments: null);
+
+          var consumer = new EventingBasicConsumer(channelReservations);
+
+          consumer.Received += (model, ea) =>
+          {
+            Console.WriteLine("received");
+
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            var reservation = JsonSerializer.Deserialize<Reservation>(message);
+            Console.WriteLine($"xd");
+
+            db.Add(reservation);
+            db.SaveChanges();
+
+            Console.WriteLine($"haha");
+
+            channelConfirmations.BasicPublish(exchange: "",
+                                      routingKey: "confirmations",
+                                      basicProperties: null,
+                                      body: body);
+
+            Console.WriteLine($"[x] Sent {message}");
+          };
+
+          channelReservations.BasicConsume(queue: "reservations",
+              autoAck: true,
+              consumer: consumer);
+
+
+          Console.WriteLine(" Press [enter] to exit.");
+          Console.ReadLine();
+        };
+      }
     }
+  }
 }
