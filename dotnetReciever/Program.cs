bc14ed2c9f848b75000790050adeab2fa9d5f718
Rasmus <rasmuswium@hotmail.com>
2022-04-28 13:47:10 +0200

    Added dotnet projects

---
+++ dotnetReciever/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetReciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ dotnetSender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
